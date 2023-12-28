36c07fd506b8d1a9b3e031f76b31852616838a84
Kha Dinh <khadinh@g.skku.edu>
2023-12-28 05:12:58 +0000

    update build_image and add working config
    Signed-off-by: Kha Dinh <khadinh@g.skku.edu>

--- build_image.sh
+++ build_image.sh
@@ -7,8 +7,8 @@ OUT_ISO=kernel.iso
 CMD_LINE=app-${UK_APP}.cmdl
 BINARY=build/${UK_APP}_qemu-x86_64
 
-if [ -f $CMD_LINE ]; then
-  echo "$UK_APP" > $CMD_LINE
+if [ ! -f $CMD_LINE ]; then
+  echo "$UK_APP netdev.ipv4_addr=172.44.0.2 netdev.ipv4_gw_addr=172.44.0.1 netdev.ipv4_subnet_mask=255.255.255.0 --" > $CMD_LINE
 fi
 
 rm -rf $OUT_ISO
